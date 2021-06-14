a0f30ca2db85056378dad3a281d101a3289e32e2
Tanguy-jpggit config –listgit config --global user.name Tanguy-jpg <TanguyCarnot@gmail.com>
2021-06-14 10:52:18 +0200

    Interface ok et exos commencés

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // script
+
+let variable1 = prompt('ma variable 1 = ?');
+let variable2 = prompt('ma variable 2 = ?');
+
+console.log('Variable 1:' + variable1)
+console.log('Variable 2:' + variable2)
+
+function swapVariable(){
+    let v1Value = variable1
+    let v2Value = variable2
+    
+    variable1 == v2Value
+    variable2 == v1Value
+
+    console.log('Variable 1:' + variable1)
+    console.log('Variable 2:' + variable2)
+}
